91466cf4347b6bb75f23361dd7613d6e99191f18
agussdangelo <agustinagustindangelo@hotmail.com>
2024-09-19 20:34:19 -0300

    Se agrego el nombre del programa

--- src/main/java/com/tallerwebi/infraestructura/RepositorioEgresoImpl.java
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioEgresoImpl.java
@@ -3,13 +3,14 @@ package com.tallerwebi.infraestructura;
 import com.tallerwebi.dominio.Egreso;
 import com.tallerwebi.dominio.RepositorioEgreso;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 
 public class RepositorioEgresoImpl implements RepositorioEgreso {
 
     private SessionFactory sessionFactory;
 
     @Override
-    public Egreso buscarMontoEgreso(Integer montoEgreso, Integer id) {
+    public Egreso buscarMontoEgreso(Double montoEgreso, Integer id) {
         return null;
     }
 
@@ -20,7 +21,9 @@ public class RepositorioEgresoImpl implements RepositorioEgreso {
 
     @Override
     public Egreso buscar(Integer montoABuscar) {
-        return null;
+        return (Egreso) sessionFactory.getCurrentSession().createCriteria(Egreso.class)
+                .add(Restrictions.eq("monto", montoABuscar))
+                .uniqueResult();
     }
 
     @Override
