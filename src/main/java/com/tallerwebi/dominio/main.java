d2e3143b01684a08a36702f11cdcf4dba225270e
agussdangelo <agustinagustindangelo@hotmail.com>
2024-09-25 20:17:50 -0300

    Se agrego la estructura de base de datos

--- src/main/java/com/tallerwebi/dominio/main.java
+++ src/main/java/com/tallerwebi/dominio/main.java
@@ -1,4 +1,32 @@
 package com.tallerwebi.dominio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class main {
+
+    public static Connection conexionBD(String bd){
+        Connection conexion;
+        String host = "jdbc:mysql://127.0.0.1:3306";
+        String user = "root";
+        String pass = "";
+
+        System.out.println("Cargando...");
+
+        try {
+            conexion = DriverManager.getConnection(host+bd, user, pass);
+            System.out.println("Conexion exitosa");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return conexion;
+    }
+
+    public static void main(String[] args) {
+        Connection bd = conexionBD("BaseDeDatosApiTallerWeb1");
+    }
+
 }
