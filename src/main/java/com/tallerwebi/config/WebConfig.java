369099357d4cc271010136d0bfa5fd90b2f5ea0a
agussdangelo <agustinagustindangelo@hotmail.com>
2024-09-26 17:12:57 -0300

    Se pudo conectar la base de datos MySQL con Java correctamente

--- src/main/java/com/tallerwebi/config/WebConfig.java
+++ src/main/java/com/tallerwebi/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.tallerwebi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // O el puerto de tu frontend
+                .allowedOrigins("http://127.0.0.1:3306") // O el puerto de tu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
--- src/main/java/com/tallerwebi/dominio/Main.java
+++ src/main/java/com/tallerwebi/dominio/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://127.0.0.1:3306/BaseDeDatosApiTallerWeb1?serverTimezone=UTC";
         String userName = "root";
-        String password = "";
+        String password = "fghdqr";
 
         System.out.println("Cargando...");
 
@@ -15,13 +15,13 @@ public class Main {
             Connection conexion = DriverManager.getConnection(url, userName, password);
             Statement statement = conexion.createStatement();
 
-            ResultSet resultado = statement.executeQuery("SELECT * FROM Usuario");
+            ResultSet resultado = statement.executeQuery("SELECT * FROM Egreso");
 
             while (resultado.next()) {
-                System.out.println(resultado.getString("id_usuario") + " | " +
-                        resultado.getString("email") + " | " +
-                        resultado.getString("contrasenia") + " | " +
-                        resultado.getString("rol"));
+                System.out.println(resultado.getString("id_egreso") + " | " +
+                        resultado.getString("monto") + " | " +
+                        resultado.getString("descripcion") + " | " +
+                        resultado.getString("fecha"));
             }
 
             System.out.println("Conexion exitosa");
@@ -30,7 +30,7 @@ public class Main {
             statement.close();
             resultado.close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e.getMessage());
         }
     }
 
