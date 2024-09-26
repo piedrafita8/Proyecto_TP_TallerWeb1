3cd3341c2efcfaa9bf7f79e82182a4049a4e0fc8
agussdangelo <agustinagustindangelo@hotmail.com>
2024-09-26 16:43:00 -0300

    .

---
+++ src/main/java/com/tallerwebi/dominio/excepcion/ObjetivoNoEncontrado.java
@@ -0,0 +1,8 @@
+package com.tallerwebi.dominio.excepcion;
+
+public class ObjetivoNoEncontrado extends Exception {
+
+    public ObjetivoNoEncontrado(String message) {
+        super(message);
+    }
+}
