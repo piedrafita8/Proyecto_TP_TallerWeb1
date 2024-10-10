4ec8428ea9c6d545d1b52149517e79d9c9373c72
piedrafita8 <santiagopiedrafita@gmail.com>
2024-10-10 00:00:28 -0300

    agregar egresos e ingresos a la vista esquema

--- src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
@@ -1,8 +1,10 @@
 package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.interfaces.ServicioEgreso;
+import com.tallerwebi.dominio.interfaces.ServicioIngreso;
 import com.tallerwebi.dominio.interfaces.ServicioLogin;
 import com.tallerwebi.dominio.models.Egreso;
+import com.tallerwebi.dominio.models.Ingreso;
 import com.tallerwebi.dominio.models.Usuario;
 import com.tallerwebi.dominio.excepcion.UsuarioExistente;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +22,14 @@ import java.util.List;
 public class ControladorLogin {
 
     private ServicioLogin ServicioLogin;
-    private ServicioEgreso servicioEgreso;
+    private ServicioEgreso ServicioEgreso;
+    private ServicioIngreso ServicioIngreso;
 
     @Autowired
-    public ControladorLogin(ServicioLogin ServicioLogin, ServicioEgreso servicioEgreso){
-        this.ServicioLogin = ServicioLogin;
-        this.servicioEgreso = servicioEgreso;
+    public ControladorLogin(ServicioLogin servicioLogin, ServicioEgreso servicioEgreso, ServicioIngreso servicioIngreso){
+        this.ServicioLogin = servicioLogin;
+        this.ServicioEgreso = servicioEgreso;
+        this.ServicioIngreso = servicioIngreso;
     }
 
     @RequestMapping("/login")
@@ -74,8 +78,10 @@ public class ControladorLogin {
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public ModelAndView irAHome() {
         ModelAndView modelAndView = new ModelAndView("index");
-        List<Egreso> egresos = servicioEgreso.getAllEgresos();
+        List<Egreso> egresos = ServicioEgreso.getAllEgresos();
+        List<Ingreso> ingresos = ServicioIngreso.getAllIngresos();
         modelAndView.addObject("egresos", egresos);
+        modelAndView.addObject("ingresos", ingresos);
         return modelAndView;
     }
 
