716305a470640ecfe2a702ed44dd4160a069b862
agussdangelo <agustinagustindangelo@hotmail.com>
2024-10-17 18:09:39 -0300

    Se soluciono el error de la vista contacto

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorContacto.java
@@ -0,0 +1,13 @@
+package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorContacto {
+
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto";  // Esto hace referencia a contacto.html dentro de /templates.
+    }
+}
