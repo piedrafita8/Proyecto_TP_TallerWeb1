4f13e282e8b58a3a8fab7d6d3d079d6a2b9df5cb
agussdangelo <agustinagustindangelo@hotmail.com>
2024-10-22 18:31:31 -0300

    Se agrego funcionalidad de calendario, pero no del jetty

--- src/main/webapp/resources/core/js/gastos.js
+++ src/main/webapp/resources/core/js/gastos.js
@@ -1,21 +1,38 @@
-
 // JavaScript para actualizar el valor de la categoría
 function seleccionarCategoria(categoria) {
     document.getElementById("categoriaSeleccionada").value = categoria;
 }
 
-    var categorias = document.getElementById("categorias");
-    var botones = document.getElementsByClassName("btn");
+var categorias = document.getElementById("categorias");
+var botones = document.getElementsByClassName("btn");
    for (var i = 0; i < botones.length; i++) {
-   botones[i].addEventListener("click", function (){
-
-       var current = document.getElementsByClassName("active");
-       current[0].className = current[0].className.replace("active", "");
-       this.className += " active" ;
-
-
-   });
+        botones[i].addEventListener("click", function (){
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace("active", "");
+            this.className += " active" ;
+        });
    }
+
+function guardarFechaGasto() {
+    const fechaSeleccionadaGasto = document.getElementById('fecha').value;
+
+    if (fechaSeleccionadaGasto) {
+        // Obtener las fechas existentes del sessionStorage
+        let fechasGastos = JSON.parse(sessionStorage.getItem('fechasGastos')) || [];
+
+        console.log("Fecha seleccionada:", fechaSeleccionadaGasto); // Debugging
+
+        // Agregar la nueva fecha
+        fechasGastos.push(fechaSeleccionadaGasto);
+
+        console.log("Fechas actuales:", fechasGastos); // Debugging
+
+        // Guardar nuevamente el array en sessionStorage
+        sessionStorage.setItem('fechasGastos', JSON.stringify(fechasGastos));
+    }
+}
+
+
 
 
 
