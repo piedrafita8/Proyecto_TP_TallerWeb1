dbc7e05a915cf24ff87d2dcca574da141a33abf8
piedrafita8 <santiagopiedrafita@gmail.com>
2024-11-04 22:17:53 -0300

    nuevo servicio para usuario

---
+++ src/main/java/com/tallerwebi/dominio/interfaces/ServicioUsuario.java
@@ -0,0 +1,8 @@
+package com.tallerwebi.dominio.interfaces;
+
+import com.tallerwebi.dominio.models.Usuario;
+
+public interface ServicioUsuario {
+    Usuario obtenerUsuarioPorId(Long id);
+    void actualizarSaldo(Long id, Double nuevoSaldo);
+}
